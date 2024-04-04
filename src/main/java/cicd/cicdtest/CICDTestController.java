fb8658529d5c0ef03e169cdd1b71a4114417d8c9
parksoonoh <62783397+parksoonoh@users.noreply.github.com>
2024-04-04 20:51:36 +0900

    dockerfile root

--- src/main/java/cicd/cicdtest/CICDTestController.java
+++ src/main/java/cicd/cicdtest/CICDTestController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CICDTestController {
     @GetMapping("/")
     public String homepage(){
-        return "This is Home Page";
+        return "This is second Home Page";
     }
     @GetMapping("/hello")
     public String helloPage(){
         return "Hello World!";
+    }
+
+    @GetMapping("/hashing_master")
+    public String specialPage(){
+        return "You are hashing master!";
     }
 }
