7b75ccf80f45b3e846b9c885f92136581339c54a
parksoonoh <62783397+parksoonoh@users.noreply.github.com>
2024-04-08 14:48:52 +0900

    update MemberRepository to JPARepository

--- src/main/java/cicd/cicdtest/repository/MemberRepository.java
+++ src/main/java/cicd/cicdtest/repository/MemberRepository.java
@@ -1,4 +1,9 @@
 package cicd.cicdtest.repository;
 
-public interface MemberRepository {
+import org.springframework.stereotype.Repository;
+
+import java.lang.reflect.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String>{
 }
