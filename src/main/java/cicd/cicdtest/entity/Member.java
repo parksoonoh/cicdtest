18a00fb0d3c641107fda66c1a06f5aa549694f0c
parksoonoh <62783397+parksoonoh@users.noreply.github.com>
2024-04-08 14:16:03 +0900

    create Member entity

---
+++ src/main/java/cicd/cicdtest/entity/Member.java
@@ -0,0 +1,26 @@
+package cicd.cicdtest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+    @Id
+    @Column(name = "member_id")
+    private String id;
+
+    private String password;
+    private String name;
+
+    public Member(String id, String password, String name) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+    }
+}
