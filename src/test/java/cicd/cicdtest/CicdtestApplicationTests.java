e466a89fd7a9cdc989a1cbcbae3d0dc45d1a6614
parksoonoh <62783397+parksoonoh@users.noreply.github.com>
2024-04-08 17:56:44 +0900

    update develop

--- src/test/java/cicd/cicdtest/CicdtestApplicationTests.java
+++ src/test/java/cicd/cicdtest/CicdtestApplicationTests.java
@@ -3,11 +3,7 @@ package cicd.cicdtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class CicdtestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
